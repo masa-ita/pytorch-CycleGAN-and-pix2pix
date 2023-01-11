5f4fd2a2bcc0e26bd2377abd555c3144375c26cb
Masatoshi Itagaki <itagakim@gmail.com>
2023-01-11 20:56:22 +0900

    fix to run train's main()

--- train.py
+++ train.py
@@ -81,3 +81,6 @@ def main(opt):
             model.save_networks(epoch)
 
         print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
+
+if __name__ == '__main__':
+    main()
