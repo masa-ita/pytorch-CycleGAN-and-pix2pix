df50789bab715d74d149962d7917a867559aa4bb
Masatoshi Itagaki <itagakim@gmail.com>
2023-11-05 18:11:27 +0900

    fix transform for B and maskB cropping

--- data/masked_dataset.py
+++ data/masked_dataset.py
@@ -72,13 +72,19 @@ class MaskedDataset(BaseDataset):
                                          params={"crop_pos": (crop_pos_x, crop_pos_y),
                                                  "flip":True}, 
                                          grayscale=(input_nc == 1))
-        self.transform_B = get_transform(self.opt, grayscale=(output_nc == 1))
+        self.transform_B = get_transform(self.opt, 
+                                         params={"crop_pos": (crop_pos_x, crop_pos_y),
+                                                 "flip":True}, 
+                                         grayscale=(output_nc == 1))
         self.transform_maskA = get_transform(self.opt,
                                              params={"crop_pos": (crop_pos_x, crop_pos_y),
                                                  "flip":True}, 
                                              grayscale=(input_nc == 1),
                                              mask=True)
-        self.transform_maskB = get_transform(self.opt, grayscale=(output_nc == 1), mask=True)
+        self.transform_maskB = get_transform(self.opt, 
+                                             params={"crop_pos": (crop_pos_x, crop_pos_y),
+                                                 "flip":True}, 
+                                             grayscale=(output_nc == 1), mask=True)
         # apply image transformation
         A = self.transform_A(A_img)
         B = self.transform_B(B_img)
