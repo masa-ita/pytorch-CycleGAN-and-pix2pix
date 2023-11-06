9d031e97808be9bc4452d4ba98552f48eddaf285
Masatoshi Itagaki <itagakim@gmail.com>
2023-11-06 21:51:46 +0900

    oyu_masked_cyclegan_run_02 restored

--- models/test_model.py
+++ models/test_model.py
@@ -41,7 +41,6 @@ class TestModel(BaseModel):
         self.real = input['A'].to(self.device)
         self.image_paths = input['A_paths']
         self.mask = input['maskA'].to(self.device)
-        print(self.mask)
 
     def forward(self):
         """Run forward pass."""
